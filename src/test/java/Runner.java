8ee509c1d32c5bebde6a2546b1bb7fb3390848e6
Nicolas Ooliveira <nicolash.oliveira@outlook.com>
2022-11-17 21:45:20 -0300

    Atualização

--- src/test/java/Runner.java
+++ src/test/java/Runner.java
@@ -1,15 +1,18 @@
 import org.junit.runner.RunWith;
 
-import cucumber.api.CucumberOptions;
-import cucumber.api.SnippetType;
+
 import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import io.cucumber.junit.CucumberOptions.SnippetType;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = "pretty", 
         monochrome = true, 
         snippets = SnippetType.CAMELCASE, 
-        dryRun = false, strict = true)
+        dryRun = false
+        //strict = true
+        )
 public class Runner {
 	
 
